b50f3134a2702f35654a1afd2c11143cf12fe48e
rayk1m02 <hkapray@gmaill.com>
2024-08-09 14:18:22 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello darkness my old friend'));
+app.get('/', (req, res) => res.send('Testing added Review stage'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
